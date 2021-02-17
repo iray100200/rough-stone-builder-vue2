2f60c02f5f5424adcaf6e45ed4f5b7f80355f0c7
iray100200 <iray100200@hotmail.com>
2021-02-17 14:03:04 +0800

    update

---
+++ README.md
@@ -0,0 +1,23 @@
+# rough-stone-builder-vue2
+
+## A Vue2 Builder for Rough-Stone Prject
+
+### Install
+
+```
+npm install rough-stone-builder-vue2@latest -g
+```
+
+### Build your whole vue2 project using following scripts
+
+```javascript
+const build = require('rough-stone-builder-vue2')
+
+build('./src/main.js')
+```
+
+### Or
+
+```bash
+rs-build-vue2 --entry ./src/main.js
+```
---
+++ cmd/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander')
+const build = require('../build/build')
+const program = new Command()
+
+program
+  .option('-e, --entry [value]', 'webpack entry path')
+  .option('-o, --outputPath <value>', 'webpack output path')
+  .action(function (options) {
+    console.log(options)
+    build(options.entry, options.outputPath)
+  })
+
+program.parse()
